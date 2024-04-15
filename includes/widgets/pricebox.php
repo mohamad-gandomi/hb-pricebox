ddbc9a306bfdfa57e146a1c55d8b6cd0fce9d87d
gandomi <m.gandomi1995@gmail.com>
2024-04-15 12:56:16 +0330

    add html structure

--- includes/widgets/pricebox.php
+++ includes/widgets/pricebox.php
@@ -15,7 +15,7 @@ class Elementor_Price_Box_Widget extends \Elementor\Widget_Base {
 
 	public function __construct($data = [], $args = null) {
 		parent::__construct($data, $args);
-		//wp_register_style( 'hb-pricebox', HBPB_PDU . 'includes/assets/css/widgets/pricebox.css' , array(), '1.0.0' );
+		wp_register_style( 'hb-pricebox', HBPB_PDU . 'includes/assets/css/widgets/pricebox.css' , array(), '1.0.0' );
 		//wp_register_script( 'hb-pricebox', HBPB_PDU . 'includes/assets/js/widgets/pricebox.js', ['elementor-frontend'], '1.0.0', true );
 	}
 
@@ -133,6 +133,150 @@ class Elementor_Price_Box_Widget extends \Elementor\Widget_Base {
 	 */
 	protected function register_controls() {
 
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => esc_html__( 'Content', 'hb-price-box' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'pricebox_icon',
+			[
+				'label' => esc_html__( 'Icon', 'hb-price-box' ),
+				'type' => \Elementor\Controls_Manager::ICONS,
+				'default' => [
+					'value' => 'fas fa-circle',
+					'library' => 'fa-solid',
+				],
+			]
+		);
+
+		$this->add_control(
+			'pricebox_title',
+			[
+				'label' => esc_html__( 'Title', 'hb-price-box' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Buy Host', 'hb-price-box' ),
+				'placeholder' => esc_html__( 'Type your title here', 'hb-price-box' ),
+			]
+		);
+
+		$this->add_control(
+			'pricebox_subtitle',
+			[
+				'label' => esc_html__( 'Subtitle', 'hb-price-box' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Types of shared web hosting services', 'hb-price-box' ),
+				'placeholder' => esc_html__( 'Type your title here', 'hb-price-box' ),
+			]
+		);
+
+		$this->add_control(
+			'pricebox_image',
+			[
+				'label' => esc_html__( 'Choose Image', 'hb-price-box' ),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+				'default' => [
+					'url' => \Elementor\Utils::get_placeholder_image_src(),
+				],
+			]
+		);
+
+		$this->add_control(
+			'pricebox_description',
+			[
+				'label' => esc_html__( 'Description', 'hb-price-box' ),
+				'type' => \Elementor\Controls_Manager::WYSIWYG,
+				'default' => esc_html__( 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.', 'hb-price-box' ),
+				'placeholder' => esc_html__( 'Type your description here', 'hb-price-box' ),
+			]
+		);
+
+		$this->add_control(
+			'pricebox_list',
+			[
+				'label' => esc_html__( 'Price Box List', 'hb-price-box' ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'prevent_empty' => false,
+				'fields' => [
+					[
+						'name' => 'list_icon', // Changed from pricebox_list_icon to list_icon
+						'label' => esc_html__( 'Icon', 'hb-price-box' ),
+						'type' => \Elementor\Controls_Manager::ICONS,
+					],
+					[
+						'name' => 'list_text', // Changed from pricebox_list_text to list_text
+						'label' => esc_html__( 'Title', 'hb-price-box' ),
+						'type' => \Elementor\Controls_Manager::TEXT,
+					],
+				],
+				'default' => [
+					[
+						'list_icon' => [
+							'value' => 'fas fa-check',
+							'library' => 'fa-solid',
+						],
+						'list_text' => esc_html__( 'Best price available', 'hb-price-box' ),
+					],
+					[
+						'list_icon' => [
+							'value' => 'fas fa-check',
+							'library' => 'fa-solid',
+						],
+						'list_text' => esc_html__( '24 hour support', 'hb-price-box' ),
+					],
+					[
+						'list_icon' => [
+							'value' => 'fas fa-check',
+							'library' => 'fa-solid',
+						],
+						'list_text' => esc_html__( 'Dedicated servers', 'hb-price-box' ),
+					],
+					[
+						'list_icon' => [
+							'value' => 'fas fa-check',
+							'library' => 'fa-solid',
+						],
+						'list_text' => esc_html__( 'High speed and optimized', 'hb-price-box' ),
+					],
+				],
+				'title_field' => '{{{ list_text }}}',
+			]
+		);
+
+		$this->add_control(
+			'pricebox_price',
+			[
+				'label' => esc_html__( 'Price', 'hb-price-box' ),
+				'type' => \Elementor\Controls_Manager::NUMBER,
+				'min' => 0,
+				'step' => 1,
+				'default' => 200,
+			]
+		);
+
+		$this->add_control(
+			'pricebox_button_text',
+			[
+				'label' => esc_html__( 'Button', 'hb-price-box' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Buy Host', 'hb-price-box' ),
+				'placeholder' => esc_html__( 'Type your title here', 'hb-price-box' ),
+			]
+		);
+
+		$this->add_control(
+			'pricebox_button_link',
+			[
+				'label' => esc_html__( 'Button Link', 'hb-price-box' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+			]
+		);
+
+		$this->end_controls_section();
+
 	}
 
 	/**
@@ -145,5 +289,63 @@ class Elementor_Price_Box_Widget extends \Elementor\Widget_Base {
 	 */
 	protected function render() {
 
+		$settings = $this->get_settings_for_display();
+
+			?>
+			<div class="price-box">
+
+				<?php if($settings['pricebox_icon']): ?>
+				<div class="icon"><?php \Elementor\Icons_Manager::render_icon( $settings['pricebox_icon'], [ 'aria-hidden' => 'true' ] ); ?></div>
+				<?php endif; ?>
+
+				<?php if($settings['pricebox_title']): ?>
+				<div class="title"><?php echo $settings['pricebox_title']; ?></div>
+				<?php endif; ?>
+
+				<?php if($settings['pricebox_subtitle']): ?>
+				<div class="subtitle"><?php echo $settings['pricebox_subtitle']; ?></div>
+				<?php endif; ?>
+
+				<?php if($settings['pricebox_image']): ?>
+				<div class="image">
+					<img src="<?php echo $settings['pricebox_image']['url']; ?>" alt="<?php echo $settings['pricebox_image']['url']; ?>">
+				</div>
+				<?php endif; ?>
+
+				<?php if($settings['pricebox_description']): ?>
+				<div class="description"><?php echo $settings['pricebox_description']; ?></div>
+				<?php endif; ?>
+
+				<?php if($settings['pricebox_list']): ?>
+				<div class="list">
+					<ul>
+						<?php foreach (  $settings['pricebox_list'] as $list ): ?>
+							<li class="list-item">
+								<div class="list-icon"><?php \Elementor\Icons_Manager::render_icon( $list['list_icon'], [ 'aria-hidden' => 'true' ] ); ?></div>
+								<p><?php echo $list['list_text']; ?></p>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+				<?php endif; ?>
+
+				<?php if($settings['pricebox_price']): ?>
+				<div class="price">
+					شروع قیمت از <?php echo $settings['pricebox_price']; ?> هزار تومان
+				</div>
+				<?php endif; ?>
+
+				<?php if($settings['pricebox_button_text']): ?>
+				<div class="link">
+					<a href="<?php echo $settings['pricebox_button_link']; ?>"><?php echo $settings['pricebox_button_text']; ?></a>
+				</div>
+				<?php endif; ?>
+
+			</div>
+			<?php
+
+			
 	}
+
+	
 }
